994c5c9fd80045dbca1d054e2c48d5baa4d1e377
Joruiz-VLC-es <127895977+Joruiz-VLC-es@users.noreply.github.com>
2026-07-02 20:36:47 +0200

    Add Subtitle dataclass for handling subtitles

---
+++ models.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+
+@dataclass(slots=True)
+class Subtitle:
+    index: int
+    start: str
+    end: str
+    lines: list[str] = field(default_factory=list)
+
+    @property
+    def text(self) -> str:
+        return "\n".join(self.lines)
+
+    @text.setter
+    def text(self, value: str) -> None:
+        self.lines = value.splitlines()
